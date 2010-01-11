f2ad6ed0222e2c4f13e968329d474078a03dd629
davetayls <dave@the-taylors.org>
2010-01-11 10:21:12 +0000

    update release script

--- release.bat
+++ release.bat
@@ -1,2 +1,7 @@
+del release\*.* /Q/S
+copy core\*.js release\
+copy core\*.css release\
+copy core\readme.txt release\
+xcopy sample\*.* release\sample\ /e
 jsCombiner "C:\Projects\juxtapo\release\"
 pause
