5332e096d02ede832643724cd1ec3a785d0ee295
davetayls <dave@the-taylors.org>
2010-02-11 15:36:19 +0000

    added goto spreadsheet link

--- plugins/juxtapo.gspreadsheet/juxtapo.gspreadsheet.js
+++ plugins/juxtapo.gspreadsheet/juxtapo.gspreadsheet.js
@@ -6,31 +6,38 @@
  */
 (function() {
 
-	juxtapo.initComplete(function() {
-	});
+    juxtapo.initComplete(function() {
+    });
 
-	juxtapo.gspreadsheet = {
-		addTemplates : function(spreadSheetRef) {
+    juxtapo.gspreadsheet = {
+        addTemplates: function(spreadSheetRef) {
 
-			var jsFile = juxtapo.utils.getJsLocation("juxtapo.gspreadsheet.js") + "gspreadsheet.js";
-			juxtapo.utils.requireResource(jsFile);
+            var jsFile = juxtapo.utils.getJsLocation("juxtapo.gspreadsheet.js") + "gspreadsheet.js";
+            juxtapo.utils.requireResource(jsFile);
 
-			GSpreadsheet.load(spreadSheetRef, {
-				index : 'designRef'
-			}, function(gs) {
-				gs.each(function(row) {
-					var settings = {};
-					settings.data = {};
-					for ( var x = 0; x < gs.headers.length; x++) {
-						settings.data[gs.headers[x]] = row[gs.headers[x]];
-					}
-					var styleJSON = '{' + row.style + '}';
-					eval('settings.style = '+ styleJSON);
+            var goTo = new juxtapo.ui.toolbtn();
+            goTo.text("Open Spreadsheet");
+            jQuery(goTo.container)
+                .attr("href", "http://spreadsheets.google.com/ccc?key=" + spreadSheetRef + "&hl=en_GB")
+                .attr("target","_blank");
+            juxtapo.thumbs.appendToToolbarRight(goTo.container)
 
-					juxtapo.addTemplate(row.path, row.imageurl, settings);
-				});
-			});
+            GSpreadsheet.load(spreadSheetRef, {
+                index: 'designRef'
+            }, function(gs) {
+                gs.each(function(row) {
+                    var settings = {};
+                    settings.data = {};
+                    for (var x = 0; x < gs.headers.length; x++) {
+                        settings.data[gs.headers[x]] = row[gs.headers[x]];
+                    }
+                    var styleJSON = '{' + row.style + '}';
+                    eval('settings.style = ' + styleJSON);
 
-		}
-	};
+                    juxtapo.addTemplate(row.path, row.imageurl, settings);
+                });
+            });
+
+        }
+    };
 })();
