48a738d729f646224ec8029879be70179efd3239
Dave Taylor <dave@the-taylors.org>
2010-04-01 13:21:02 +0100

    updated the testsuite to use relative paths

--- tests/juxtapo-tests/juxtapo-testsuite/testsuite.cs
+++ tests/juxtapo-tests/juxtapo-testsuite/testsuite.cs
@@ -12,12 +12,13 @@ namespace juxtapo_testsuite
     {
         private ISelenium selenium;
         private StringBuilder verificationErrors;
-		private String browserUrl = "file:///D:/projects/juxtapo/tests/";
+        private String browserUrl = "file:///"+Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\..\\")).Replace("\\","/");
 
         [SetUp]
         public void SetupTest()
         {
 			Console.WriteLine(browserUrl);
+            Console.WriteLine(Directory.GetCurrentDirectory());
             selenium = new DefaultSelenium("localhost", 4444, "*firefox", browserUrl);
             selenium.Start();
             verificationErrors = new StringBuilder();
@@ -37,14 +38,12 @@ namespace juxtapo_testsuite
         }
 
         [Test]
-        public void TheNewTest()
+        public void VerifyQunitTests()
         {
             selenium.Open("TestSuite.html");
             selenium.WaitForPageToLoad("30000");
-			selenium.WaitForCondition("typeof(window.QUnitDone) != 'undefined' && window.QUnitDone","30000");
-			Console.WriteLine(selenium.GetHtmlSource());
-			Boolean IsPass = selenium.IsElementPresent("//h2[@class='qunit-pass']");
-			Console.WriteLine(IsPass);
+            selenium.WaitForCondition("typeof(window.QUnitDone) != 'undefined' && window.QUnitDone", "30000");
+            Boolean IsPass = selenium.IsElementPresent("//h2[@class='qunit-pass']");
             Assert.IsTrue(IsPass);
         }
     }
