9fd23edcc57ac48f6758f44e205f437914233317
Dave Tayor <dave@the-taylors.org>
2010-06-27 21:21:36 +0100

    deleted combiner from project

--- combiner/win/core/juxtapo-combiner/test.bat
+++
@@ -1,9 +0,0 @@
-del test /s/q
-md test
-del test\*.js
-copy *.js test\
-md test\subdir
-copy subdir\*.js test\subdir\
-bin\Debug\juxtapo-combiner -root:"test\" -vars:JUXTAPO_VERSION=0.7;VARTWO=blah
-REM bin\Debug\juxtapo-combiner "test\"
-pause
